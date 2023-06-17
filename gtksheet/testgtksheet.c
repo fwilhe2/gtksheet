aa2759a885b0a73257d575f9f595a40c17653fff
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2023-06-17 18:34:43 +0200

    add toolbar

--- gtksheet/testgtksheet.c
+++ gtksheet/testgtksheet.c
@@ -7,25 +7,35 @@
 #include "gtksheet.h"
 
 static void
-activate(GtkApplication *app,
-         gpointer user_data)
+activate(GtkApplication *app, gpointer user_data)
 {
-  GtkWidget *window;
-  GtkWidget *sheet;
-
-  window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "Window");
+  GtkWidget *window = gtk_application_window_new(app);
+  gtk_window_set_title(GTK_WINDOW(window), "Spreadsheet");
   gtk_window_set_default_size(GTK_WINDOW(window), 1024, 768);
 
-  sheet = gtk_sheet_new(20, 20, "Edit table");
-//   GTK_SHEET_SET_FLAGS(sheet, GTK_SHEET_AUTORESIZE);
+  GtkWidget *cellAddress = gtk_label_new("foo");
+  GtkWidget *formula = gtk_data_entry_new();
 
-  gtk_container_add (GTK_CONTAINER (window), sheet);
+  GtkWidget *sheet = gtk_sheet_new(20, 20, "Sheet 1");
+  //   GTK_SHEET_SET_FLAGS(sheet, GTK_SHEET_AUTORESIZE);
+
+  GtkWidget *box_spreadsheet = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+  gtk_box_set_homogeneous(GTK_BOX(box_spreadsheet), FALSE);
+
+  GtkWidget *box_toolbar = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+  gtk_box_set_homogeneous(GTK_BOX(box_spreadsheet), TRUE);
+
+  gtk_container_add(GTK_BOX(box_toolbar), cellAddress);
+  gtk_container_add(GTK_BOX(box_toolbar), formula);
+
+  gtk_container_add(GTK_BOX(box_spreadsheet), box_toolbar);
+  gtk_container_add(GTK_BOX(box_spreadsheet), sheet);
+
+  gtk_container_add(GTK_CONTAINER(window), box_spreadsheet);
   gtk_widget_show_all(window);
 }
 
-int main(int argc,
-         char **argv)
+int main(int argc, char **argv)
 {
   GtkApplication *app;
   int status;
