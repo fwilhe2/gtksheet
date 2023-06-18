2a14e5d4f9c6f2971aa2d962472895ba663bf401
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2023-06-18 10:36:44 +0200

    Improve layout

--- gtksheet/testgtksheet.c
+++ gtksheet/testgtksheet.c
@@ -6,44 +6,51 @@
 #include <string.h>
 #include "gtksheet.h"
 
+void on_entry_changed(GtkEntry *entry, gpointer user_data)
+{
+    const gchar *formula = gtk_entry_get_text(entry);
+    printf("Formula: %s\n", formula);
+    fflush(stdout);
+}
+
 static void
 activate(GtkApplication *app, gpointer user_data)
 {
-  GtkWidget *window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "Spreadsheet");
-  gtk_window_set_default_size(GTK_WINDOW(window), 1024, 768);
+    GtkWidget *window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Spreadsheet");
+    gtk_window_set_default_size(GTK_WINDOW(window), 1024, 768);
 
-  GtkWidget *cellAddress = gtk_label_new("foo");
-  GtkWidget *formula = gtk_data_entry_new();
+    // Create the main grid layout
+    GtkWidget *grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(window), grid);
 
-  GtkWidget *sheet = gtk_sheet_new(20, 20, "Sheet 1");
-  //   GTK_SHEET_SET_FLAGS(sheet, GTK_SHEET_AUTORESIZE);
+    // Create the label
+    GtkWidget *label = gtk_label_new("R1C1");
+    gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
 
-  GtkWidget *box_spreadsheet = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-  gtk_box_set_homogeneous(GTK_BOX(box_spreadsheet), FALSE);
+    // Create the formula entry
+    GtkWidget *entry = gtk_entry_new();
+    g_signal_connect(entry, "changed", G_CALLBACK(on_entry_changed), NULL);
+    gtk_grid_attach(GTK_GRID(grid), entry, 1, 0, 1, 1);
 
-  GtkWidget *box_toolbar = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
-  gtk_box_set_homogeneous(GTK_BOX(box_spreadsheet), TRUE);
+    // Create the spreadsheet area
+    GtkWidget *spreadsheet = gtk_sheet_new(20, 20, "Sheet 1");
+    gtk_widget_set_hexpand(spreadsheet, TRUE);
+    gtk_widget_set_vexpand(spreadsheet, TRUE);
+    gtk_grid_attach(GTK_GRID(grid), spreadsheet, 0, 1, 2, 1);
 
-  gtk_container_add(GTK_BOX(box_toolbar), cellAddress);
-  gtk_container_add(GTK_BOX(box_toolbar), formula);
-
-  gtk_container_add(GTK_BOX(box_spreadsheet), box_toolbar);
-  gtk_container_add(GTK_BOX(box_spreadsheet), sheet);
-
-  gtk_container_add(GTK_CONTAINER(window), box_spreadsheet);
-  gtk_widget_show_all(window);
+    gtk_widget_show_all(window);
 }
 
 int main(int argc, char **argv)
 {
-  GtkApplication *app;
-  int status;
+    GtkApplication *app;
+    int status;
 
-  app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-  status = g_application_run(G_APPLICATION(app), argc, argv);
-  g_object_unref(app);
+    app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
 
-  return status;
+    return status;
 }
